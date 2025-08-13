b7ac63ff1dd3ce793fd0317a89c7ef1d0f9b899e
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2025-08-13 06:36:17 +0200

    Allowed origins from appsettings

--- src/ProjectTracker.Api/Program.cs
+++ src/ProjectTracker.Api/Program.cs
@@ -1,16 +1,22 @@
 using ProjectTracker.Infrastructure;
 using ProjectTracker.Infrastructure.Database.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+var allowedOrigins = builder.Configuration
+    .GetSection("AllowedOrigins")
+    .Get<string[]>() ?? Array.Empty<string>();
+
+
 builder.Services.AddInfrastructure(builder.Configuration);
 
 builder.Services.AddCors(o =>
-  o.AddDefaultPolicy(p => p
-    .AllowAnyHeader()
-    .AllowAnyMethod()
-    .WithOrigins("https://localhost:7778", "http://localhost:7777"))); // Put in appsettings
+    o.AddDefaultPolicy(p => p
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins(allowedOrigins)));
 
 
 var app = builder.Build();
