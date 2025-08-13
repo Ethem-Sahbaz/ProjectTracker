daf1540641a2990c88d9ab585a45c27dd4e7f62a
Ethem-Sahbaz <ethemsahbaz@outlook.com>
2025-08-13 07:27:23 +0200

    Added projects endpoint

---
+++ src/ProjectTracker.Api/Features/Projects/GetProjects.cs
@@ -0,0 +1,14 @@
+﻿using ProjectTracker.Api.Abstractions.Endpoints;
+
+namespace ProjectTracker.Api.Features.Projects;
+
+internal sealed class GetProjects : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/projects", () =>
+        {
+            return "Testprojects...";
+        });
+    }
+}
